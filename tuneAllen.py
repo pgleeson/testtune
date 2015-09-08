682d7c8c2c4d350d8a8efe74035ec4561a23c67e
Padraig Gleeson <p.gleeson@gmail.com>
2015-09-08 18:27:30 +0100

    Adding izh based example

--- tuneAllen.py
+++ tuneAllen.py
@@ -19,34 +19,44 @@ parameters = ['cell:RS/channelDensity:Na_all/mS_per_cm2',
               'cell:RS/channelDensity:LeakConductance_all/mS_per_cm2',
               'cell:RS/erev_id:LeakConductance_all/mV',
               'cell:RS/erev_id:Na_all/mV',
-              'cell:RS/erev_id:Kd_all/mV']
+              'cell:RS/erev_id:Kd_all/mV',
+              'cell:RS/erev_id:IM_all/mV']
 
 #above parameters will not be modified outside these bounds:
-min_constraints = [20,   1,    1e-6,  1e-6, -100, 50, -100]
-max_constraints = [100,  30,   4,     0.1,  -70,  60,  -70]
+min_constraints = [20,   1,    1e-6,  0.001, -100, 50, -100, -100]
+max_constraints = [100,  25,   4,     0.1,     -70,  60, -70,  -70]
 
 
 ref = 'Pop0/0/RS/v:'
 average_maximum = ref+'average_maximum'
 average_minimum = ref+'average_minimum'
 mean_spike_frequency = ref+'mean_spike_frequency'
-first_spike_time = ref+'first_spike_time'
+#first_spike_time = ref+'first_spike_time'
 average_last_1percent = ref+'average_last_1percent'
 
 weights = {average_maximum: 1,
            average_minimum: 1,
-           mean_spike_frequency: 1,
-           first_spike_time: 1,
+           mean_spike_frequency: 5,
            average_last_1percent: 1}
 
 target_data = {average_maximum: 33.320915,
                average_minimum: -44.285,
                mean_spike_frequency: 26.6826,
-               first_spike_time: 226,
                average_last_1percent: -80}
 
 simulator  = 'jNeuroML_NEURON'
 #simulator  = 'jNeuroML'
+
+parameters_iz = ['izhikevich2007Cell:RS/a/per_ms',
+                 'izhikevich2007Cell:RS/b/nS',
+                 'izhikevich2007Cell:RS/c/mV',
+                 'izhikevich2007Cell:RS/d/pA',
+                 'izhikevich2007Cell:RS/vr/mV',
+                 'izhikevich2007Cell:RS/vt/mV',
+                 'izhikevich2007Cell:RS/vpeak/mV',]
+                 
+min_constraints_iz = [0.01, -5, -65, 10,  -100, -70, 0]
+max_constraints_iz = [0.2,  20, -50, 400, -40,  0,   60]
         
 def run_one_optimisation(ref,
                      seed,
@@ -57,7 +67,15 @@ def run_one_optimisation(ref,
                      mutation_rate,
                      num_elites,
                      simulator,
-                     nogui):
+                     nogui,
+                     neuroml_file =     'models/RS/AllenTest.net.nml',
+                     target =           'network_RS',
+                     parameters =       parameters,
+                     max_constraints =  max_constraints,
+                     min_constraints =  min_constraints,
+                     weights =          weights,
+                     target_data =      target_data,
+                     dt =               0.025,):
                          
     ref = '%s__s%s_p%s_m%s_s%s_o%s_m%s_e%s'%(ref,
                      seed,
@@ -69,15 +87,15 @@ def run_one_optimisation(ref,
                      num_elites)           
 
     run_optimisation(prefix =           ref, 
-                     neuroml_file =     'models/RS/AllenTest.net.nml',
-                     target =           'network_RS',
+                     neuroml_file =     neuroml_file,
+                     target =           target,
                      parameters =       parameters,
                      max_constraints =  max_constraints,
                      min_constraints =  min_constraints,
                      weights =          weights,
                      target_data =      target_data,
                      sim_time =         1500,
-                     dt =               0.025,
+                     dt =               dt,
                      seed =             seed,
                      population_size =  population_size,
                      max_evaluations =  max_evaluations,
@@ -116,6 +134,49 @@ if __name__ == '__main__':
         t, v = cont.run_individual(sim_vars, show=(not nogui))
         
         
+    elif '-izhone' in sys.argv:
+        
+        simulator  = 'jNeuroML'
+        
+        cont = NeuroMLController('AllenIzhTest', 
+                                 'models/RS/AllenIzh.net.nml',
+                                 'network_RS',
+                                 1500, 
+                                 0.01, 
+                                 simulator)
+                                 
+        example_vars = {'izhikevich2007Cell:RS/a/per_ms': 0.01}
+
+        sim_vars = OrderedDict(example_vars)
+                                
+                                 
+        t, v = cont.run_individual(sim_vars, show=(not nogui))
+        
+        
+        
+    elif '-izhquick' in sys.argv:
+        
+        simulator  = 'jNeuroML'
+        
+        run_one_optimisation('AllenIzh',
+                            1234,
+                            population_size =  40,
+                            max_evaluations =  300,
+                            num_selected =     20,
+                            num_offspring =    15,
+                            mutation_rate =    0.2,
+                            num_elites =       3,
+                            simulator =        simulator,
+                            nogui =            nogui,
+                            dt =               0.05,
+                            neuroml_file =     'models/RS/AllenIzh.net.nml',
+                            target =           'network_RS',
+                            parameters =       parameters_iz,
+                            max_constraints =  max_constraints_iz,
+                            min_constraints =  min_constraints_iz)
+        
+        
+        
     elif '-quick' in sys.argv:
 
         run_one_optimisation('AllenTestQ',
@@ -133,12 +194,12 @@ if __name__ == '__main__':
         
         run_one_optimisation('AllenTest',
                             1234,
-                            population_size =  30,
-                            max_evaluations =  200,
-                            num_selected =     20,
+                            population_size =  20,
+                            max_evaluations =  100,
+                            num_selected =     15,
                             num_offspring =    15,
                             mutation_rate =    0.1,
-                            num_elites =       3,
+                            num_elites =       1,
                             simulator =        simulator,
                             nogui =            nogui)
         
